189612493d56ee2deaa7f8e2be04cb595f8151ec
EvidLens <joram.mburun@gmail.com>
2026-07-08 20:40:00 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding: '40px', textAlign: 'center'}}>
+      <h1>🚀 Next.js Works!</h1>
+      <p>No more Vite position 292 error</p>
+    </div>
+  )
+}
