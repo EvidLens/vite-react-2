cff953e4de394e705ab91ed6bbd8fc68ce1f9c68
EvidLens <joram.mburun@gmail.com>
2026-07-09 13:43:41 +0300

    Create page.js

---
+++ app/chat/page.js
@@ -0,0 +1,10 @@
+'use client'
+export default function ChatPage() {
+  return (
+    <div style={{padding: '40px', textAlign: 'center'}}>
+      <h1>Ask Lens AI</h1>
+      <p>Chat coming soon</p>
+      <button onClick={() => window.location.href = '/'}>Back Home</button>
+    </div>
+  )
+}
