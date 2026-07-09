a2910c4a3c72390c4bc399e9d0c1d8c41135da4c
EvidLens <joram.mburun@gmail.com>
2026-07-09 13:57:16 +0300

    Create page.js

---
+++ app/login/page.js
@@ -0,0 +1,10 @@
+'use client'
+export default function LoginPage() {
+  return (
+    <div style={{padding: '40px', textAlign: 'center'}}>
+      <h1>Login</h1>
+      <p>Login page coming soon</p>
+      <button onClick={() => window.location.href = '/'}>Back Home</button>
+    </div>
+  )
+}
