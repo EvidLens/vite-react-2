c4dc2f039419bbc65aba7aeb6b88e1b217bcdf7c
EvidLens <joram.mburun@gmail.com>
2026-07-09 12:32:47 +0300

    Delete app directory

--- app/lanes/page.js
+++
@@ -1,4 +0,0 @@
-'use client'
-export default function Lanes() {
-  return <div style={{padding: '20px'}}><h1>All 9 Lanes</h1><button onClick={() => window.location.href = '/'}>← Home</button></div>
-}
