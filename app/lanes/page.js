ab0719ea01f65a40d736017fac03208332351e13
EvidLens <joram.mburun@gmail.com>
2026-07-09 13:50:49 +0300

    Create page.js

---
+++ app/lanes/page.js
@@ -0,0 +1,10 @@
+'use client'
+export default function LanesPage() {
+  return (
+    <div style={{padding: '40px', textAlign: 'center'}}>
+      <h1>9 Lanes</h1>
+      <p>Coming Soon</p>
+      <button onClick={() => window.location.href = '/'}>Back Home</button>
+    </div>
+  )
+}
